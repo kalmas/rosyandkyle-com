5ac22bce90fc0063b72a1cc34da747353fc1d49f
kalmas <kylealmas@gmail.com>
2022-01-22 22:23:55 -0500

    cleanup

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -2,9 +2,13 @@
     import helpers from "../helpers";
 </script>
 
-<div class="container">
-    <h1 class="display-1 text-center">{helpers.ourNames()} Are Getting Married!</h1>
-    <div class="text-center"><a class="display-3 link-primary" href="/rsvp">Please RSVP Here!</a></div>
-    <img class="img-fluid mb-2" alt="Kyle and Rosy" src="rosyandkyle.png" />
-</div>
+<div class="container text-center">
+    <h1 class="display-1">
+        {helpers.ourNames()} Are Getting Married!
+    </h1>
 
+    <h2 class="display-3">
+        <a class="link-primary" href="/rsvp">Please RSVP Here!</a>
+    </h2>
+    <img class="img-fluid mb-2" alt="Kyle and Rosy" src="rosyandkyle.webp" />
+</div>
